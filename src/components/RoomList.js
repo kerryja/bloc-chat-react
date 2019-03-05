d5c41e5d700be4b31c3889132fadb1d81c62903c
Kerry <kerry.jackson108@gmail.com>
2019-03-05 13:31:25 -0500

    updated form

--- src/components/RoomList.js
+++ src/components/RoomList.js
@@ -19,17 +19,30 @@ class RoomList extends Component {
     });
   }
 
+  createRoom() {
+    this.roomsRef.push({
+      name: this.state.rooms.name
+    });
+  }
+
   render() {
     return (
-      <div>
-        <ul>
-          {this.state.rooms.map(room => (
-            <li>{room.name}</li>
-          ))}
-        </ul>
-      </div>
+      <form onSubmit={this.createRoom.bind(this)}>
+        <input type="text" ref={input => (this.state.rooms.name = input)} />
+        <input type="submit" />
+      </form>
     );
   }
 }
 
 export default RoomList;
+
+/*<div>
+        <ul>
+          {this.state.rooms.map(room => {
+               (<li>{room.name}</li>);
+          })}
+        </ul>
+      </div>
+    )
+        }*/
